3c132b9aa41ac84721c7cab83ff70f4aca05736e
lesc <lesc@gmail.com>
2026-09-08 14:26:37 +0500

    PaTIENTS LIST ADDED

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "HOSPITAL MANAGEMENT SYSTEM";
 echo "<a href = 'add-patient.php'>PATIENT PAGE</a>";
-
+echo "<a href = 'patients.php'>PATIENT LIST</a>";
 ?>
---
+++ patients.php
@@ -0,0 +1,6 @@
+<?php
+echo "<br><h1>PATINETS LIST</h1>";
+echo "<ul>";
+ echo "<li>AHMED</li>"  ;      
+echo"</ul>";
+?>
