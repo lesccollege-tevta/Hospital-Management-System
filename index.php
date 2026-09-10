a1dd1754670548074af80fe7c40f06510dd120b6
lesc <lesc@gmail.com>
2026-09-10 15:11:52 +0500

    Construction heading added

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 echo "HOSPITAL MANAGEMENT SYSTEM";
 echo "<a href = 'add-patient.php'>PATIENT PAGE</a>";
 echo "<a href = 'patients.php'>PATIENT LIST</a>";
+echo "HOSPITAL MANAGEMENT SYSTEM is under construction";
 ?>
